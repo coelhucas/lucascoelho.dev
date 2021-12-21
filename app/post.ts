0c6a50fb2c355dd532a1967b3083eb978d0ab150
Lucas Coelho <lucascoelhodacosta@gmail.com>
2021-12-21 16:53:44 -0300

    chore: revert fs change

--- app/post.ts
+++ app/post.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import fs from "fs/promises";
+import { promises as fs } from "fs";
 import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
 import { marked, Renderer } from "marked";
