e8e46673b4a802ae86ea765f2ccc2c4ca9177040
Lucas Coelho <lucascoelhodacosta@gmail.com>
2022-06-14 20:42:59 -0300

    chore: attempt to use footnotes

--- app/post.ts
+++ app/post.ts
@@ -20,6 +20,42 @@ const postsPath = process.env.NETLIFY ?
   path.join(__dirname, '../../../..', 'posts') : path.join(process.cwd(), 'posts');
 
 const renderer = new Renderer();
+const footnoteMatch = /^\[\^([^\]]+)\]:([\s\S]*)$/;
+const referenceMatch = /\[\^([^\]]+)\](?!\()/g;
+const referencePrefix = "marked-fnref";
+const footnotePrefix = "marked-fn";
+
+const footnoteTemplate = (ref: string, text: string) => {
+  return `<sup id="${footnotePrefix}:${ref}">${ref}</sup>${text}`;
+};
+
+const referenceTemplate = (ref: string) => {
+  return `<sup id="${referencePrefix}:${ref}"><a href="#${footnotePrefix}:${ref}">${ref}</a></sup>`;
+};
+
+const interpolateReferences = (text: string) => {
+  return text.replace(referenceMatch, (_, ref) => {
+    return referenceTemplate(ref);
+  });
+}
+
+const interpolateFootnotes = (text: string) => {
+  return text.replace(footnoteMatch, (_, value, text) => {
+    return footnoteTemplate(value, text);
+  });
+}
+renderer.paragraph = (text: string) => {
+  return marked.Renderer.prototype.paragraph.apply(renderer, [
+    interpolateReferences(interpolateFootnotes(text))
+  ]);
+};
+
+renderer.text = (text: string) => {
+  return marked.Renderer.prototype.text.apply(renderer, [
+    interpolateReferences(interpolateFootnotes(text))
+  ]);
+};
+
 renderer.heading = (text: string, level: number) => {
   const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
 
