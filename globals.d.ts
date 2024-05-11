a5ab8a99cd7bb96fcfa3660c799f4e6503d7d28d
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-05-11 15:43:29 -0300

    chore: add env vars typing

--- globals.d.ts
+++ globals.d.ts
@@ -3,6 +3,8 @@ export {};
 declare global {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
+    NETLIFY: string;
+    GA_TRACKING_ID: string;
   }
   interface Process {
     env: ProcessEnv;
