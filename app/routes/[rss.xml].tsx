ec3663b9290ba3cc561f4519d247faf7bf779692
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-05-12 14:08:34 -0300

    fix: cdata usage for contenet

--- app/routes/[rss.xml].tsx
+++ app/routes/[rss.xml].tsx
@@ -24,14 +24,11 @@ export function generateRss({
 }): string {
   return `<?xml version="1.0" encoding="UTF-8"?>
 
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>${title}</title>
     <description>${description}</description>
     <link>${link}</link>
-    <language>en-us</language>
-    <ttl>60</ttl>
-    <atom:link href="https://lucascoelho.dev/rss.xml" rel="self" type="application/rss+xml" />
     ${entries
       .map(
         (entry) => `
@@ -40,7 +37,7 @@ export function generateRss({
         <pubDate>${entry.pubDate}</pubDate>
         <link>${entry.link}</link>
         ${entry.guid ? `<guid isPermaLink="false">${entry.guid}</guid>` : ""}
-        <content:encoded>${entry.content}</content:encoded>
+        <content:encoded><![CDATA[${entry.content}]]></content:encoded>
       </item>`,
       )
       .join("")}
@@ -55,6 +52,7 @@ export const loader: LoaderFunction = async () => {
   const entries = await Promise.all(
     posts.map(async (post) => {
       const { html } = await getPost(post.slug);
+      console.log({html: sanitizeHtml(html)})
       return {
         pubDate: new Date(post.date).toUTCString(),
         title: post.title,
