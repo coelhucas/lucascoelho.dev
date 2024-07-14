6ad64489ea26659db5d71f66ea59a9180e93c31e
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-07-14 16:38:45 -0300

    chore: add lambda favicon

--- app/root.tsx
+++ app/root.tsx
@@ -75,6 +75,10 @@ function Layout({ children }: { children: ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%22-24 8 84 84%22><text y=%22.9em%22 font-size=%2290%22>&lambda;</text></svg>"
+        ></link>
         <Meta />
         <Links />
       </head>
