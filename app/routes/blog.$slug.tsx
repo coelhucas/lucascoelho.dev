527627208e6095a3932285657aca89f15c0c301a
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-07-14 14:04:21 -0300

    feat: perform forbidden magic to inject script

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -11,6 +11,7 @@ import type { SerializedPost } from "~/utils/post";
 import { getPost } from "~/utils/post";
 
 import { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { useEffect, useRef } from "react";
 import type { Theme } from "remix-themes";
 import { useTheme } from "remix-themes";
 import globalMeta from "~/utils/global-meta";
@@ -68,6 +69,43 @@ const CommentsSection = ({
   const location = useLocation();
   const canonicalUrl = url + location.pathname;
 
+  const scriptParent = useRef<HTMLDivElement>(null);
+  // GAMBETA DO MAL
+  const script = `<script>
+  /**
+  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+  var disqus_config = function () {
+  this.page.url = "${canonicalUrl}";  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.identifier = "${slug}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+  };
+
+  // Gambeta sinistra
+  // https://ourcodeworld.com/articles/read/1606/how-to-reset-disqus-color-schema-automatically
+  (function() {
+  document.addEventListener('colorSchemeChanged', function (e) {
+  setTimeout(() => {
+      if (document.readyState == 'complete') {
+        DISQUS.reset({ reload: true, config: disqus_config });
+      }
+    }, 0)
+  })
+  })();
+
+  (function() { // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
+  s.src = 'https://lucascoelho.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+  })();
+  </script>`;
+
+  useEffect(() => {
+    const range = document.createRange();
+    const documentFragment = range.createContextualFragment(script);
+    scriptParent.current?.append(documentFragment);
+  }, []);
+
   // const commentSection = useUtterances(
   //   {
   //     src: "https://utteranc.es/client.js",
@@ -81,48 +119,16 @@ const CommentsSection = ({
   //   React.createRef(),
   // );
   return (
-    <span style={{}}>
+    <div ref={scriptParent}>
       <div id="disqus_thread"></div>
 
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-  /**
-  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-  var disqus_config = function () {
-  this.page.url = "${canonicalUrl}";  // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = "${slug}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-  };
-
-  // Gambeta sinistra
-  // https://ourcodeworld.com/articles/read/1606/how-to-reset-disqus-color-schema-automatically
-  (function() {
-    document.addEventListener('colorSchemeChanged', function (e) {
-      setTimeout(() => {
-          if (document.readyState == 'complete') {
-            DISQUS.reset({ reload: true, config: disqus_config });
-          }
-        }, 0)
-    })
-  })();
-
-  (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://lucascoelho.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-  })();
-  `,
-        }}
-      />
       <noscript>
         Please enable JavaScript to view the{" "}
         <a href="https://disqus.com/?ref_noscript">
           comments powered by Disqus.
         </a>
       </noscript>
-    </span>
+    </div>
   );
 };
 
