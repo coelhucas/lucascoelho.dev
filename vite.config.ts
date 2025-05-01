88c59dbe862dc1e04143b6991f2856cc06a1fca2
Lucas Coelho <lucascoelhodacosta@gmail.com>
2025-05-01 19:42:58 -0300

    chore: update netlify integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   plugins: [reactRouter(), , netlifyPlugin(), tsconfigPaths()],
